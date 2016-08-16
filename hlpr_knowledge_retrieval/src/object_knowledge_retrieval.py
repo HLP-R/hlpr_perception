d4e64b9e0984f7eb36839cc12111ec201f16d14a
TescaF <tesca.fitzgerald@gmail.com>
2016-08-16 09:07:49 -0500

    added filename param

--- hlpr_knowledge_retrieval/src/object_knowledge_retrieval.py
+++ hlpr_knowledge_retrieval/src/object_knowledge_retrieval.py
@@ -10,12 +10,11 @@ import rospy
 from hlpr_object_labeling.msg import LabeledObjects
 from hlpr_knowledge_retrieval.msg import ObjectKnowledge
 
-affFilename = "/home/tesca/data/affordance_table.npy"
-propFilename = "/home/tesca/data/property_table.npy"
+affFilename = None 
+propFilename = None
 
 class lookup:
     def __init__(self):
-	rospy.init_node('labeling', anonymous=True)
 	self.affDictionary = np.load(affFilename).item()
 	self.propDictionary = np.load(propFilename).item()
         self.subscriber = rospy.Subscriber("/beliefs/labels", LabeledObjects, self.cbLabels, queue_size = 1)
@@ -41,9 +40,22 @@ class lookup:
         msg.properties = propList
         msg.labels = labels
 	self.knowPub.publish(msg)
+def get_param(name, value=None):
+    private = "~%s" % name
+    if rospy.has_param(private):
+        return rospy.get_param(private)
+    elif rospy.has_param(name):
+        return rospy.get_param(name)
+    else:
+        return value
 
 def main(args):
-    global lkup
+    global lkup, affFilename, propFilename
+    rospy.init_node('knowledge_retrieval', anonymous=True)
+    filename = get_param("knowledge_data_loc")
+    affFilename = filename + "affordance_table.npy"
+    propFilename = filename + "property_table.npy"
+    print "reading from " + filename
     lkup = lookup()
     rospy.spin()
 
